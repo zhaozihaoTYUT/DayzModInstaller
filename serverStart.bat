936bc3eaed2b5f51bcca003dcc63039c478cbecb
Andy <zhaozihaoTYUT@163.com>
2025-07-05 14:41:27 +0800

    Create serverStart.bat

---
+++ serverStart.bat
@@ -0,0 +1,22 @@
+﻿@echo off
+cd /d %~dp0
+COLOR 06
+ECHO =============================
+ECHO =                           =
+ECHO =                           =
+ECHO =   BiliBili -QianNiuWei-   =
+ECHO =          Server           =
+ECHO =                           =
+ECHO =                           =
+ECHO =============================
+:srcds
+echo (%time%) srcds Server Launching Successful !!!
+start /wait "AlphaGo" DayZServer_x64.exe ^
+    -config=ServerDZ.cfg ^
+    -port=2056 ^
+    -profiles=D:\你的路径\DayZServer\profiles ^
+    "-mod=" ^
+    "-servermod=";" ^
+    -adminlog -doLogs -freezecheck -NoFilePatch -tradeching -netLog
+echo (%time%) WARNING: srcds Something in Server wrong!!! Restarting now…..
+goto srcds
